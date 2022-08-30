205d70e38d9194923d8afb321fc5cd283bcae8d0
Vitalii Aleksandrov <vitaliy0508@ukr.net>
2022-08-30 21:59:56 +0300

    add basic parcel setup and markup with styles

---
+++ src/index.js
@@ -0,0 +1,6 @@
+import './css/styles.css';
+import Notiflix from 'notiflix';
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
+
+const DEBOUNCE_DELAY = 300;
